d0276d813382a098da466fe56d6fc34e2cb3b3d9
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-06-12 15:51:41 -0300

    imdb/about page

--- imdb/src/app/components/MenuItem.tsx
+++ imdb/src/app/components/MenuItem.tsx
@@ -7,11 +7,9 @@ interface MenuItemProps {
 }
 
 export default function MenuItem(props: MenuItemProps) {
-  const Icon = props.icon;
   return (
     <div>
       <Link href={props.address} className="flex flex-row mx-4 lg:mx-6 hover:text-amber-600">
-        <Icon className="text-2xl sm:hidden mx-4"/>
         <p className="hidden sm:inline my-2 text-sm">{props.title}</p>
       </Link>
     </div>
