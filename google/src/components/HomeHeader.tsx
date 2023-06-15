c15696d966f64b4517f36d75564fd15a90d4b5dc
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-06-15 06:53:54 -0300

    update google

---
+++ google/src/components/HomeHeader.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function HomeHeader() {
+  return (
+    <header>
+        <div className="">
+            <Link href='https://mail.google.com'>
+                Gmail
+            </Link>
+
+        </div>
+    </header>
+  )
+  
+}
